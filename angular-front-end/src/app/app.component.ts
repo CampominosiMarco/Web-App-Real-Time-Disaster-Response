c1a5343b248d55026b935c5c033d7735a87dc6bd
Marco Campominosi <marco.campominosi@gmail.com>
2023-11-01 21:52:01 +0100

    form test

--- angular-front-end/src/app/app.component.ts
+++ angular-front-end/src/app/app.component.ts
@@ -5,6 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'TEST Titolo';
+
+  creator_name = 'Marco Campominosi';
+  image_campo = '/assets/CM-InnovationLab.it_logo_completo.png';
+
+
+
+  onClickMarco(){
+    console.log("CIAONE!")
+  }
+
+  value_Marco = "";
+
 }
--- angular-front-end/src/app/app.module.ts
+++ angular-front-end/src/app/app.module.ts
@@ -4,13 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms'; //Serve per il Two-way binding dell'input form
+
+
+//Fondamentale chiamare la classe dentro il componente
+import { FormComponentComponent } from './form-component/form-component.component';
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    FormComponentComponent  //Fondamentale indicare tutti i componenti da lanciare all'avvio
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule         //Serve per il binding
+
   ],
   providers: [],
   bootstrap: [AppComponent]
